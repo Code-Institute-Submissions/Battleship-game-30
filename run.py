42e10c3be0da8ebe22b77a3ee8173b6110261925
Adams Sawiba <Addy305@protonmail.com>
2023-06-17 07:13:32 +0000

    create a grid size and display grid

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# function to create an empty grid
+def create_grid(size):
+    # take a grid size for n*n and append O in-place
+    grid = []
+    for _ in range(size):
+        row = ["O"] * size
+        grid.append(row)
+    return grid
+
+# function to display the grid
+def display_grid(grid):
+    for row in grid:
+        # with spaced row and column display grid
+        print(" ".join(row))
